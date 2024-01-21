933f5e4fe786231400b2691c6184489b641cc058
kuro <toshras22@gmail.com>
2024-01-21 11:57:29 +0900

    Initial Commit

---
+++ SampleSwift/SampleSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSwiftApp.swift
+//  SampleSwift
+//
+//  Created by 白石明裕 on 2024/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
